29c04662627c917abd0ff85e2e5c870a1f42a7e6
marcyli <47973003+marcyli@users.noreply.github.com>
2019-02-25 19:42:05 +0800

    Update XMLUtil.java
    修复存在的XXE漏洞， 详见微信支付官方公告：https://pay.weixin.qq.com/wiki/doc/api/micropay.php?chapter=23_5

--- alipay-payment/src/main/java/cn/aposoft/ecommerce/payment/alipay/util/XMLUtil.java
+++ alipay-payment/src/main/java/cn/aposoft/ecommerce/payment/alipay/util/XMLUtil.java
@@ -71,6 +71,53 @@ public class XMLUtil {
 			throws SAXException, IOException, ParserConfigurationException {
 		// 构建xml解析工厂
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		String FEATURE = null;
+		try {
+			// This is the PRIMARY defense. If DTDs (doctypes) are disallowed, almost all XML entity attacks are prevented
+			// Xerces 2 only - http://xerces.apache.org/xerces2-j/features.html#disallow-doctype-decl
+			FEATURE = "http://apache.org/xml/features/disallow-doctype-decl";
+			dbf.setFeature(FEATURE, true);
+
+			// If you can't completely disable DTDs, then at least do the following:
+			// Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-general-entities
+			// Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-general-entities
+			// JDK7+ - http://xml.org/sax/features/external-general-entities 
+			FEATURE = "http://xml.org/sax/features/external-general-entities";
+			dbf.setFeature(FEATURE, false);
+
+			// Xerces 1 - http://xerces.apache.org/xerces-j/features.html#external-parameter-entities
+			// Xerces 2 - http://xerces.apache.org/xerces2-j/features.html#external-parameter-entities
+			// JDK7+ - http://xml.org/sax/features/external-parameter-entities 
+			FEATURE = "http://xml.org/sax/features/external-parameter-entities";
+			dbf.setFeature(FEATURE, false);
+
+			// Disable external DTDs as well
+			FEATURE = "http://apache.org/xml/features/nonvalidating/load-external-dtd";
+			dbf.setFeature(FEATURE, false);
+
+			// and these as well, per Timothy Morgan's 2014 paper: "XML Schema, DTD, and Entity Attacks"
+			dbf.setXIncludeAware(false);
+			dbf.setExpandEntityReferences(false);
+
+			// And, per Timothy Morgan: "If for some reason support for inline DOCTYPEs are a requirement, then 
+			// ensure the entity settings are disabled (as shown above) and beware that SSRF attacks
+			// (http://cwe.mitre.org/data/definitions/918.html) and denial 
+			// of service attacks (such as billion laughs or decompression bombs via "jar:") are a risk."
+
+			// remaining parser logic
+		} catch (ParserConfigurationException e) {
+			// This should catch a failed setFeature feature
+			logger.info("ParserConfigurationException was thrown. The feature '" +
+			FEATURE + "' is probably not supported by your XML processor.");
+		}
+		catch (SAXException e) {
+			// On Apache, this should be thrown when disallowing DOCTYPE
+			logger.warning("A DOCTYPE was passed into the XML document");
+		}
+		catch (IOException e) {
+			// XXE that points to a file that doesn't exist
+			logger.error("IOException occurred, XXE may still possible: " + e.getMessage());
+		}
 		// 构建具体的xml解析器
 		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
 		InputStream is = getStringStream(xml);
